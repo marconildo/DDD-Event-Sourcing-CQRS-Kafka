b65a3bb6b0fdbf49e034f0442156b82cb1b99a36
Michael Gray <michael.gray@synx.com>
2020-02-20 21:22:33 +0000

    Add IgnoreMemberAttribute to accompany ValueObject

---
+++ src/BuildingBlocks/MikeGrayCodes.BuildingBlocks.Domain/ValueObjects/IgnoreMemberAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeGrayCodes.BuildingBlocks.Domain.ValueObjects
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
