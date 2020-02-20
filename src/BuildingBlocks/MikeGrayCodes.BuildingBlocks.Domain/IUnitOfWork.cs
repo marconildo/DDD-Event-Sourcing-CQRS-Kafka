e94d58276446584ba9907b07cd502d9e9d07a518
Michael Gray <michael.gray@synx.com>
2020-02-20 21:22:52 +0000

    Add applicationuser repository

---
+++ src/BuildingBlocks/MikeGrayCodes.BuildingBlocks.Domain/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MikeGrayCodes.BuildingBlocks.Domain
+{
+    public interface IUnitOfWork
+    {
+        Task<int> Complete(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Services/Authentication/MikeGrayCodes.Authentication.Domain/Entities/ApplicationUser/IApplicationUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikeGrayCodes.Authentication.Domain.Entities.ApplicationUser
+{
+    public interface IApplicationUserRepository
+    {
+        Task Add(ApplicationUser applicationUser);
+    }
+}
